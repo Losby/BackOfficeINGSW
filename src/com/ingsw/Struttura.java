1c2c07fecb94d5d6ce31bc4e51178267d544f005
Losby <losbjr@hotmail.it>
2020-10-06 17:35:11 +0200

    add new fields and get/set/toString methods

--- src/com/ingsw/Struttura.java
+++ src/com/ingsw/Struttura.java
@@ -12,17 +12,54 @@ package com.ingsw;
 public class Struttura {
     private String NomeStruttura;
     private String Indirizzo;
-    private String Prezzo;
+    private String Citta;
+    private int Prezzo;
     private String Categoria;
     private int IDStruttura;
+    private String LinkImmagine;
     
     public Struttura() {}
     
-    public Struttura(String Nome, String Indirizzo, String Prezzo, String Categoria) {
+    public Struttura(String Nome, String Indirizzo, String Citta, int Prezzo, String Categoria) {
         this.NomeStruttura = Nome;
         this.Indirizzo = Indirizzo;
+        this.Citta = Citta;
         this.Prezzo = Prezzo;
         this.Categoria = Categoria;
+    }
+    
+    public Struttura(int Codice, String Nome, String Indirizzo, String Citta, int Prezzo, String Categoria, String LinkImmagine) {
+        this.IDStruttura = Codice;
+        this.NomeStruttura = Nome;
+        this.Indirizzo = Indirizzo;
+        this.Citta = Citta;
+        this.Prezzo = Prezzo;
+        this.Categoria = Categoria;
+        this.LinkImmagine = LinkImmagine;
+    }
+    
+    public String toString() {
+        String RangePrezzo = "€";
+        if(Prezzo == 1) {
+            RangePrezzo = "€";
+        } else if (Prezzo == 2) {
+            RangePrezzo = "€€";
+        } else if (Prezzo == 3) {
+            RangePrezzo = "€€€";
+        }
+        return ""+NomeStruttura+", "+Indirizzo+", "+Citta+", "+Categoria+", "+RangePrezzo+"";
+    }
+    
+    public String getCitta() {
+        return Citta;
+    }
+    
+    public String getCategoria() {
+        return Categoria;
+    }
+    
+    public int getPrezzo() {
+        return Prezzo;
     }
     
     public String getNomeStruttura() {
