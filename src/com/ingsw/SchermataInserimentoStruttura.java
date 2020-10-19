a62557583c7650c2cbed54bc84871724e9906589
Losby <losbjr@hotmail.it>
2020-10-19 21:39:10 +0200

    add method to flush java components after an insert

--- src/com/ingsw/SchermataInserimentoStruttura.java
+++ src/com/ingsw/SchermataInserimentoStruttura.java
@@ -31,6 +31,14 @@ public class SchermataInserimentoStruttura extends javax.swing.JFrame {
         this.controller = controller;
         this.setLocationRelativeTo(null);
     }
+    
+    public void svuotaCampi() {
+        CittaField.setText("");
+        IndirizzoField.setText("");
+        NomeStrutturaField.setText("");
+        CategoriaBox.setSelectedIndex(0);
+        PrezzoBox.setSelectedIndex(0);
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -294,41 +302,6 @@ public class SchermataInserimentoStruttura extends javax.swing.JFrame {
             AvantiButton.setEnabled(true);
         }
     }//GEN-LAST:event_IndirizzoFieldMouseMoved
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserimentoStruttura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserimentoStruttura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserimentoStruttura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserimentoStruttura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SchermataInserimentoStruttura().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AvantiButton;
--- src/com/ingsw/SchermataInserisciFoto.java
+++ src/com/ingsw/SchermataInserisciFoto.java
@@ -42,6 +42,12 @@ public class SchermataInserisciFoto extends javax.swing.JFrame {
         controller.setFoto(Foto);
     }
     
+    public void svuotaCampi() {
+        DirectoryFotoLabel.setText("");
+        FotoLabel.setIcon(null);
+        FotoCaricata = null;
+    }
+    
     private void mostraFoto() {
         try {
             DirectoryFotoLabel.setText(FotoCaricata.getPath());
@@ -104,7 +110,6 @@ public class SchermataInserisciFoto extends javax.swing.JFrame {
 
         DirectoryFotoLabel.setFont(new java.awt.Font("Corbel", 0, 12)); // NOI18N
         DirectoryFotoLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        DirectoryFotoLabel.setText("jLabel3");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 3;
@@ -189,41 +194,6 @@ public class SchermataInserisciFoto extends javax.swing.JFrame {
         this.setVisible(false);
         FotoChooser.setVisible(true);
     }//GEN-LAST:event_CaricaFotoButtonActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserisciFoto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserisciFoto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserisciFoto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SchermataInserisciFoto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SchermataInserisciFoto().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton CaricaFotoButton;
